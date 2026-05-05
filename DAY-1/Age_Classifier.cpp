e5e414748d5ab0d5b22b0d07da3bac8c7e40b5d9
Yuvrajjain11 <yuvrajjain897@gmail.com>
2026-05-06 01:16:43 +0530

    First Commit

---
+++ DAY-1/Age_Classifier.cpp
@@ -0,0 +1,19 @@
+/*Write a program that takes age as input.
+Check if the person is eligible for a discount (age < 12 or >= 65)
+and if they belong to the working age group (18-64).*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cin >> age;
+
+    bool discount = (age < 12 || age >= 65);
+    bool working = (age >= 18 && age <= 64);
+    cout << "Discount Eligibe: " << discount << endl;
+    cout << "Working Age: " << working << endl;
+
+    return 0;
+}
